c0251e477fa4f513be3f468d793e9eb81a7595fe
PopcornPaws <lipschitz77@gmail.com>
2022-04-26 11:39:41 +0200

    fixed error

--- tom256/scripts/add.py
+++ tom256/scripts/add.py
@@ -4,23 +4,22 @@ from fastecdsa.curve import Curve
 
 k256 = Curve(
 		name = "k256",
-		p = 
-		a = 
-		b = 
-		q = 
-		gx = 
-		gy =
+		p = 0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f,
+		a = 0,
+		b = 7,
+		q = 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141,
+		gx = 0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798,
+		gy = 0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8,
 )
 
 
 #xs = 0xB8F0170E293FCC9291BEE2665E9CA9B25D3B11810ED68D9EA0CB440D7064E4DA
 #ys = 0x0691AA44502212591132AA6F27582B78F9976998DE355C4EE5960DB05AC0A2A3
-#xs = 0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798
-#ys = 0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8
+xs = 0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798
+ys = 0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8
 S = Point(xs, ys, curve=k256)
 
 T = 2 * S
-print(T)
 print(hex(T.x))
 print(hex(T.y))
 
--- tom256/src/point.rs
+++ tom256/src/point.rs
@@ -258,7 +258,7 @@ impl<C: Curve> Point<C> {
     }
 
     pub fn y(&self) -> &FieldElement<C> {
-        &self.x
+        &self.y
     }
 
     pub fn z(&self) -> &FieldElement<C> {
@@ -307,41 +307,59 @@ mod test {
         let tom_scalar = TomScalar::new(U256::from_u32(678910));
         let tom_point = TomPoint::GENERATOR.scalar_mul(&tom_scalar);
         assert!(tom_point.is_on_curve());
+
+        let tom_on_sec = SecPoint {
+            x: FieldElement::new(Tom256k1::GENERATOR_X),
+            y: FieldElement::new(Tom256k1::GENERATOR_Y),
+            z: FieldElement::ONE,
+        };
+
+        let sec_on_tom = TomPoint {
+            x: FieldElement::new(Secp256k1::GENERATOR_X),
+            y: FieldElement::new(Secp256k1::GENERATOR_Y),
+            z: FieldElement::ONE,
+        };
+        assert!(!tom_on_sec.is_on_curve());
+        assert!(!sec_on_tom.is_on_curve());
     }
 
     #[test]
     fn point_addition_test() {
-        //let random_double = SecPoint {
-        //    x: FieldElement::new(U256::from_be_hex(
-        //        "B8F0170E293FCC9291BEE2665E9CA9B25D3B11810ED68D9EA0CB440D7064E4DA",
-        //    )),
-        //    y: FieldElement::new(U256::from_be_hex(
-        //        "0691AA44502212591132AA6F27582B78F9976998DE355C4EE5960DB05AC0A2A3",
-        //    )),
-        //    z: FieldElement::ONE,
-        //}
-        //.double()
-        //.into_affine();
-        //assert!(random_double.is_on_curve());
-        //assert_eq!(
-        //    random_double.x().inner(),
-        //    &U256::from_be_hex("d99bdf80fe99540ed7c33669cc43ac72fa2fa2c14b731ae6758c1c17eaf7b26e")
-        //);
-        //assert_eq!(
-        //    random_double.y().inner(),
-        //    &U256::from_be_hex("cac2c38a379655150567315c7cf7f596585b577b28e03108b0d2df2b9c83af52")
-        //);
-        //assert_eq!(random_double.z().inner(), &U256::ONE);
-
-        let g2 = SecPoint::GENERATOR.double().into_affine();
+        let g2 = SecPoint::GENERATOR.double();
         assert_eq!(
             g2.x().inner(),
-            &U256::from_be_hex("c6047f9441ed7d6d3045406e95c07cd85c778e4b8cef3ca7abac09b95c709ee5")
+            &U256::from_be_hex("f40af3b6c6fdf9aa5402b9fdc39ac4b67827eb373c92077452348e044f109fc8")
         );
         assert_eq!(
             g2.y().inner(),
-            &U256::from_be_hex("1ae168fea63dc339a3c58419466ceaeef7f632653266d0e1236431a950cfe52a")
+            &U256::from_be_hex("56915849f52cc8f76f5fd7e4bf60db4a43bf633e1b1383f85fe89164bfadcbdb")
         );
+        assert_eq!(
+            g2.z().inner(),
+            &U256::from_be_hex("f8783c53dfb2a307b568a6ad931fc97023dc71cdc3eac498b0c6ba5554759a29")
+        );
+
+        let random_double = SecPoint {
+            x: FieldElement::new(U256::from_be_hex(
+                "B8F0170E293FCC9291BEE2665E9CA9B25D3B11810ED68D9EA0CB440D7064E4DA",
+            )),
+            y: FieldElement::new(U256::from_be_hex(
+                "0691AA44502212591132AA6F27582B78F9976998DE355C4EE5960DB05AC0A2A3",
+            )),
+            z: FieldElement::ONE,
+        }
+        .double()
+        .into_affine();
+        assert!(random_double.is_on_curve());
+        assert_eq!(
+            random_double.x().inner(),
+            &U256::from_be_hex("d99bdf80fe99540ed7c33669cc43ac72fa2fa2c14b731ae6758c1c17eaf7b26e")
+        );
+        assert_eq!(
+            random_double.y().inner(),
+            &U256::from_be_hex("cac2c38a379655150567315c7cf7f596585b577b28e03108b0d2df2b9c83af52")
+        );
+        assert_eq!(random_double.z().inner(), &U256::ONE);
 
         let four = SecScalar::new(U256::from_u8(4));
         let g4 = SecPoint::GENERATOR.scalar_mul(&four);
@@ -353,6 +371,14 @@ mod test {
     fn affine_point() {
         let g2 = SecPoint::GENERATOR.double();
         let g2_affine = g2.into_affine();
+        assert_eq!(
+            g2_affine.x().inner(),
+            &U256::from_be_hex("c6047f9441ed7d6d3045406e95c07cd85c778e4b8cef3ca7abac09b95c709ee5")
+        );
+        assert_eq!(
+            g2_affine.y().inner(),
+            &U256::from_be_hex("1ae168fea63dc339a3c58419466ceaeef7f632653266d0e1236431a950cfe52a")
+        );
         assert_eq!(g2_affine.z(), &FieldElement::ONE);
 
         let mut id = SecPoint::IDENTITY;
