f6250e821c01860d4316b5e579c4f8a84e2d2fba
gyozosz <gyozo.szabo.97@gmail.com>
2022-05-18 16:59:49 +0200

    Moved zkp_attest example to a test

--- tom256/src/proofs/mod.rs
+++ tom256/src/proofs/mod.rs
@@ -18,7 +18,10 @@ use rand_core::{CryptoRng, RngCore};
 use serde::{Deserialize, Serialize};
 
 // NOTE 80 is conservative but slow, 40 is faster but quite low security
+#[cfg(not(test))]
 const SEC_PARAM: usize = 60;
+#[cfg(test)]
+const SEC_PARAM: usize = 10;
 
 #[derive(Deserialize, Serialize)]
 pub struct ZkAttestProof<C: Curve, CC: Cycle<C>> {
@@ -126,5 +129,61 @@ impl<C: Curve, CC: Cycle<C>> ZkAttestProof<C, CC> {
         )?;
 
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::ZkAttestProof;
+
+    use crate::curve::{Secp256k1, Tom256k1};
+    use crate::parse::{ParsedProofInput, ProofInput};
+    use crate::pedersen::PedersenCycle;
+
+    use rand::rngs::StdRng;
+    use rand_core::SeedableRng;
+    
+    #[test]
+    fn zkp_attest_valid() {
+        let mut rng = StdRng::from_seed([14; 32]);
+        let pedersen_cycle = PedersenCycle::<Secp256k1, Tom256k1>::new(&mut rng);
+
+        let msg_hash = "0xb42062702a4acb9370edf5c571f2c7a6f448f8c42f3bfa59e622c1c064a94a14".to_string();
+        let signature = "0xb2a7ff958cd78c8e896693b7b76550c8942d6499fb8cd621efb54909f9d51da02bfaadf918f09485740ba252445d40d44440fd810dbf8a9a18049157adcdaa8c1c".to_string();
+        let address = "0x2e3Eca6005eb4e30eA51692011612554586feaC9".to_string();
+        let pubkey = "0x0418a30afe39c280d2f43f05c070988dae7fbae9cdfd5fb6461acd7657e765e172fd55b3589c74fd4987b6004465afff77b039e631a68cdc7df9cd8cfd5cbe2887".to_string();
+
+        let ring = vec![
+            "0x0e3Eca6005eb4e30eA51692011612554586feaC9".to_string(),
+            "0x1e3Eca6005eb4e30eA51692011612554586feaC9".to_string(),
+            address,
+            "0x3e3Eca6005eb4e30eA51692011612554586feaC9".to_string(),
+            "0x4e3Eca6005eb4e30eA51692011612554586feaC9".to_string(),
+        ];
+
+        let index = 2;
+
+        let proof_input = ProofInput {
+            msg_hash,
+            pubkey,
+            signature,
+            index,
+            ring,
+        };
+
+        let parsed_input: ParsedProofInput<Secp256k1, Tom256k1> = proof_input.try_into().unwrap();
+        let address_parsed = parsed_input.ring[index];
+        let address_committed = pedersen_cycle.cycle().commit(&mut rng, address_parsed);
+
+        let zkattest_proof_res = ZkAttestProof::<Secp256k1, Tom256k1>::construct(
+            &mut rng,
+            pedersen_cycle,
+            address_committed,
+            parsed_input,
+        );
+        assert!(zkattest_proof_res.is_ok());
+
+        let zkattest_proof = zkattest_proof_res.unwrap();
+        assert!(zkattest_proof.verify(&mut rng).is_ok());
     }
 }
