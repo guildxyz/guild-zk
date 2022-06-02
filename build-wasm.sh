3b0862eda087f7a66a2e66247165d95fd09c40cc
PopcornPaws <lipschitz77@gmail.com>
2022-06-02 18:34:04 +0200

    Whole workflow test.

--- build-wasm.sh
+++ build-wasm.sh
@@ -1,18 +1,14 @@
 #!/bin/sh
 
-#wasm-pack build client --target ${WASM_TARGET} --out-name index --out-dir ../wasm-output
-
-mkdir wasm-output
-touch wasm-output/bello
+wasm-pack build ${WASM_DIR} --target bundler --out-dir wasm-output
 
 cd wasm-output
 rm -f .gitignore
-git init
 git add -A
 git commit -m "Auto-generated wasm code"
-git remote add origin https://${ACCESS_HEADER}github.com/agoraxyz/agora-wasm-hub.git
-git branch -M actions-test
-git push -uf origin actions-test
+git remote add origin https://${ACCESS_HEADER}github.com/${TARGET_REPO}
+git branch -M ${TARGET_BRANCH}
+git push -uf origin ${TARGET_BRANCH}
 
 cd ..
 rm -rf wasm-output
