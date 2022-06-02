380211fc25cc3818420ffc0d5593c0b6a30087e4
PopcornPaws <lipschitz77@gmail.com>
2022-06-02 20:13:34 +0200

    test

--- build-wasm.sh
+++ build-wasm.sh
@@ -16,7 +16,7 @@ rm -f .gitignore
 git init
 git add -A
 git commit -m "Auto-generated wasm code"
-git remote add origin https://${ACCESS_HEADER}github.com/${TARGET_REPO}
+git remote add origin https://${ACCESS_HEADER}github.com/agoraxyz/agora-wasm-hub.git
 git branch -M ${TARGET_BRANCH}
 git push -uf origin ${TARGET_BRANCH}
 
