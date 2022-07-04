e1e59a4145e913afb506156af295f995ea32316d
PopcornPaws <lipschitz77@gmail.com>
2022-07-04 16:20:51 +0200

    Initial cleanup.

---
+++ agora-zkp-ecdsa/examples/verifier.rs
@@ -0,0 +1,38 @@
+use rand_core::OsRng;
+use structopt::StructOpt;
+use agora_zkp_ecdsa::curve::{Secp256k1, Tom256k1};
+use agora_zkp_ecdsa::parse::*;
+use agora_zkp_ecdsa::proofs::ZkAttestProof;
+
+use std::error::Error;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::PathBuf;
+
+#[derive(StructOpt)]
+struct Opt {
+    #[structopt(long, help = "zk ecdsa and membership proof")]
+    proof: PathBuf,
+    #[structopt(long, help = "array of public keys as string")]
+    ring: PathBuf,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut rng = OsRng;
+    let opt = Opt::from_args();
+
+    let ring_file = File::open(opt.ring)?;
+    let ring_reader = BufReader::new(ring_file);
+
+    let proof_file = File::open(opt.proof)?;
+    let proof_reader = BufReader::new(proof_file);
+
+    let ring: Ring = serde_json::from_reader(ring_reader)?;
+    let parsed_ring = parse_ring(ring)?;
+
+    let proof: ZkAttestProof<Secp256k1, Tom256k1> = serde_json::from_reader(proof_reader)?;
+
+    proof.verify(&mut rng, &parsed_ring)?;
+    println!("Proof OK");
+    Ok(())
+}
