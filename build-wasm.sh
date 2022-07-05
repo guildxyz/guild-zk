1c5568b6b4a9c668ea75d80da316f046680dd25d
PopcornPaws <lipschitz77@gmail.com>
2022-07-05 08:39:33 +0200

    Updated wasm build script.

--- build-wasm.sh
+++ build-wasm.sh
@@ -8,19 +8,19 @@ WASM_DIRS=("agora-zkp-ecdsa" "agora-zkp-triptych")
 for WASM_DIR in ${WASM_DIRS[@]}
 do
 	BRANCH_NAME=${WASM_DIR}@${TARGET_BRANCH}
-	echo ${BRANCH_NAME}
 
 	wasm-pack build ${WASM_DIR} --target bundler --out-dir ${OUT_DIR}
 
 	cd ${WASM_DIR}/${OUT_DIR}
 	rm -f .gitignore
 	git init
-	git remote add origin https://${ACCESS_HEADER_TRIMMED}@github.com/agoraxyz/agora-wasm-hub.git
+	#git remote add origin https://${ACCESS_HEADER_TRIMMED}@github.com/agoraxyz/agora-wasm-hub.git
+	git remote add origin https://github.com/agoraxyz/agora-wasm-hub.git
 	git branch -M ${BRANCH_NAME}
 	git add -A
 	git commit -m "Auto-generated wasm code"
 	git push -uf origin ${BRANCH_NAME}
-	
-	cd ..
+
+	cd ../..
 	rm -rf ${WASM_DIR}/${OUT_DIR}
 done
