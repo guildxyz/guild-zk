c2d738870f8c585763fabcff06b369028e2231b5
PopcornPaws <lipschitz77@gmail.com>
2022-08-24 19:31:17 +0200

    Started documenting steps (with Latex in docs)

--- agora-threshold-sig/src/shares.rs
+++ agora-threshold-sig/src/shares.rs
@@ -7,45 +7,58 @@ use rand_core::{CryptoRng, RngCore};
 
 #[derive(Clone, Debug)]
 pub struct Shares {
-    pub public_poly: Polynomial<G2Projective>,
-    pub encrypted_shares: Vec<EncryptedShare>,
+    /// This is the public version $A(x)$ of the privately generated
+    /// polynomial $a(x)$ with degree $t-1$, where $t$ is the threshold.
+    ///
+    /// The private polynomial is generated over a finite field $\mathbb{F}_p$
+    ///
+    /// $$a(x) = a_0 + a_1x +\ldots + a_{t - 1}x^{t - 1}$$
+    ///
+    /// with $x, a_i\in\mathbb{F_p}\ \forall i$. The public polynomial is defined as
+    ///
+    /// $$A(x) = A_0 + A_1x +\ldots + A_{t - 1}x^{t - 1}$$
+    ///
+    /// with $x\in\mathbb{F_p}$ and $A_i = g_2^{a_i}\in\mathbb{G_2}\ \forall i$.
+    pub poly: Polynomial<G2Projective>,
+    /// These are the encrypted shares of the privately generated shares $sh_i$
+    /// with $i\in[0..n - 1]$, where $n$ is the total number of participants in
+    /// the threshold cryptosystem.
+    pub esh_vec: Vec<EncryptedShare>,
 }
 
-pub type Evaluations = Vec<G2Projective>;
+pub type ShareVerificationKeys = Vec<G2Projective>;
 
 impl Shares {
     pub fn new<R: RngCore + CryptoRng>(
         rng: &mut R,
-        poly: &Polynomial<Scalar>,
+        private_poly: &Polynomial<Scalar>,
         participants: &[Participant],
     ) -> Self {
         // TODO size checks?
         // t = poly.coeffs().len()
         // n = participants.len()
-        let encrypted_shares = participants
+        let esh_vec = participants
             .iter()
             .map(|participant| {
-                let secret_share = poly.evaluate(participant.id);
+                let secret_share = private_poly.evaluate(participant.id);
                 EncryptedShare::new(rng, participant, &secret_share)
             })
             .collect();
-        let public_poly = Polynomial::new(
-            poly.coeffs()
+        let poly = Polynomial::new(
+            private_poly
+                .coeffs()
                 .iter()
                 .map(|coeff| G2Affine::generator() * coeff)
                 .collect(),
         );
 
-        Self {
-            public_poly,
-            encrypted_shares,
-        }
+        Self { poly, esh_vec }
     }
 
-    pub fn evaluations(&self, participants: &[Participant]) -> Evaluations {
+    pub fn verification_keys(&self, participants: &[Participant]) -> ShareVerificationKeys {
         participants
             .iter()
-            .map(|participant| self.public_poly.evaluate(participant.id))
-            .collect::<Vec<G2Projective>>()
+            .map(|participant| self.poly.evaluate(participant.id))
+            .collect::<ShareVerificationKeys>()
     }
 }
--- agora-threshold-sig/src/tests.rs
+++ agora-threshold-sig/src/tests.rs
@@ -1,15 +1,29 @@
 use crate::participant::Participant;
-use crate::shares::{Evaluations, Shares};
+use crate::shares::{ShareVerificationKeys, Shares};
 use agora_interpolate::Polynomial;
 use bls::{G2Affine, Scalar};
 use ff::Field;
+
+// TODO
+/*
+pub struct Node {
+    private_key: Scalar,
+    private_poly: Polynomial<Scalar>,
+    participant: Participant,
+    share_map: HashMap<Scalar, Share>,
+}
+
+    // methods?
+    shsk: Scalar, // share signing key
+    shvk: G2Affine, // share verification key
+*/
 
 #[test]
 fn dkg_23() {
     let mut rng = rand_core::OsRng;
     let secret_keys = (0..3)
         .into_iter()
-        .map(|i| Scalar::random(&mut rng))
+        .map(|_| Scalar::random(&mut rng))
         .collect::<Vec<Scalar>>();
 
     let participants = secret_keys
@@ -21,56 +35,27 @@ fn dkg_23() {
         })
         .collect::<Vec<Participant>>();
 
-    // specific because 2 coefficients are needed for a first order polynomial/participant
-    let polys = participants
+    let private_polys = participants
         .iter()
         .map(|_| Polynomial::new(vec![Scalar::random(&mut rng), Scalar::random(&mut rng)]))
         .collect::<Vec<Polynomial<Scalar>>>();
 
     // public
-    let shares = polys
+    let shares = private_polys
         .iter()
         .map(|poly| Shares::new(&mut rng, poly, &participants))
         .collect::<Vec<Shares>>();
 
     // public
-    let evaluations = shares
+    let verification_keys = shares
         .iter()
-        .map(|share| share.evaluations(&participants))
-        .collect::<Vec<Evaluations>>();
+        .map(|share| share.verification_keys(&participants))
+        .collect::<Vec<ShareVerificationKeys>>();
 
     // verify shares
-    for (evals, share) in evaluations.iter().zip(&shares) {
-        for ((participant, ev), enc) in participants.iter().zip(evals).zip(&share.encrypted_shares)
-        {
-            assert!(enc.verify(participant, &G2Affine::from(ev)))
+    for (shvk_vec, share) in verification_keys.iter().zip(&shares) {
+        for ((participant, shvk), esh) in participants.iter().zip(shvk_vec).zip(&share.esh_vec) {
+            assert!(esh.verify(participant, &G2Affine::from(shvk)))
         }
     }
-
-    // decrypt shares with own private key
-    let decrypted_shares = participants
-        .iter()
-        .zip(&secret_keys)
-        .zip(&shares)
-        .map(|((p, sk), sh)| {
-            sh.encrypted_shares
-                .iter()
-                .map(|enc| enc.decrypt(p, sk))
-                .collect::<Vec<Scalar>>()
-        })
-        .collect::<Vec<Vec<Scalar>>>();
-
-    // TODO is this correct?
-    let private_threshold_shares = decrypted_shares
-        .iter()
-        .map(|sh| {
-            let p = dbg!(Polynomial::interpolate(
-                &[participants[0].id, participants[1].id, participants[2].id],
-                sh,
-            ))
-            .unwrap();
-            p.coeffs()[0]
-        })
-        .collect::<Vec<Scalar>>();
-    assert!(false);
 }
