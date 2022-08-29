6005c7362e4b2138c878d423c7ba3e76c231611d
PopcornPaws <lipschitz77@gmail.com>
2022-08-29 19:01:28 +0200

    Partial sign tested.

--- agora-threshold-sig/src/node.rs
+++ agora-threshold-sig/src/node.rs
@@ -7,12 +7,6 @@ use rand_core::{CryptoRng, RngCore};
 
 use std::collections::BTreeMap;
 
-// TODO do this in stages (like a builder)
-// instead of having one big node with option fields
-// 1) DiscoveryPhase
-// 2) ShareCollectionPhase
-// 3) RecoveryPhase -> Actual Node
-
 #[derive(Clone, Debug)]
 pub struct Share {
     vk: G2Affine,
@@ -43,6 +37,13 @@ impl Address {
     pub fn as_bytes(&self) -> &[u8; 32] {
         &self.0
     }
+
+    pub fn to_scalar(&self) -> Scalar {
+        // NOTE unwrap is fine because a valid address
+        // can only be created from a Scalar type via
+        // hash_to_fp
+        Scalar::from_bytes(&self.0).unwrap()
+    }
 }
 
 pub struct Keypair {
@@ -55,6 +56,14 @@ impl Keypair {
         Self {
             private,
             public: G2Affine::from(G2Affine::generator() * private),
+        }
+    }
+
+    pub fn new_checked(private: Scalar, public: G2Affine) -> Result<Self, String> {
+        if public == (G2Affine::generator() * private).into() {
+            Ok(Self { private, public })
+        } else {
+            Err("invalid keypair".to_string())
         }
     }
 
@@ -73,6 +82,7 @@ impl Keypair {
     }
 }
 
+#[derive(Debug, Clone, Copy)]
 pub struct Parameters {
     nodes: usize,
     threshold: usize,
@@ -107,10 +117,19 @@ pub struct ShareCollection {
 
 pub struct Finalized {
     participants: BTreeMap<Address, G2Affine>,
-    signature_shares: Vec<G1Affine>,
+    poly_secret: Scalar,
     share_keypair: Keypair,
     global_vk: G2Affine,
-    poly_secret: Scalar,
+}
+
+impl<P> Node<P> {
+    pub fn address(&self) -> Address {
+        self.address
+    }
+
+    pub fn pubkey(&self) -> G2Affine {
+        self.keypair.public
+    }
 }
 
 impl Node<Discovery> {
@@ -130,10 +149,6 @@ impl Node<Discovery> {
         }
     }
 
-    pub fn pubkey(&self) -> G2Affine {
-        self.keypair.public
-    }
-
     pub fn collect_participant(&mut self, pubkey: G2Affine) {
         let address = Address::from(&pubkey);
         if self.phase.participants.get(&address).is_none() {
@@ -142,24 +157,32 @@ impl Node<Discovery> {
     }
 }
 
-impl Node<ShareCollection> {
-    pub fn new<R: CryptoRng + RngCore + Copy>(rng: R, node: Node<Discovery>) -> Result<Self, String> {
+impl TryFrom<Node<Discovery>> for Node<ShareCollection> {
+    type Error = String;
+    fn try_from(node: Node<Discovery>) -> Result<Self, Self::Error> {
         if node.phase.participants.len() < node.parameters.nodes {
             return Err("not enough participants collected".to_string());
         }
 
         // generate own share in this step
-        let private_coeffs = 
-            (0..node.parameters.threshold).map(|_| Scalar::random(rng)).collect::<Vec<Scalar>>();
+        let private_coeffs = (0..node.parameters.threshold)
+            .map(|_| Scalar::random(rand_core::OsRng))
+            .collect::<Vec<Scalar>>();
         let private_poly = Polynomial::new(private_coeffs);
 
-        let shares = node.phase 
+        let shares = node
+            .phase
             .participants
             .iter()
             .map(|(address, pubkey)| {
-                let secret_share = private_poly.evaluate(Scalar::from_bytes(address.as_bytes()).unwrap());
+                let secret_share = private_poly.evaluate(address.to_scalar());
                 let public_share = G2Affine::from(G2Affine::generator() * secret_share);
-                let esh = todo!();//EncryptedShare::new(rng, address, pubkey, &secret_share);
+                let esh = EncryptedShare::new(
+                    &mut rand_core::OsRng,
+                    address.as_bytes(),
+                    pubkey,
+                    &secret_share,
+                );
                 Share {
                     vk: public_share,
                     esh,
@@ -177,48 +200,29 @@ impl Node<ShareCollection> {
                 participants: node.phase.participants,
                 shares: shares_map,
                 poly_secret: private_poly.coeffs()[0],
-            }
+            },
         })
     }
 }
 
-
-
-/*
-impl TryFrom<Node<GenerateShare>>
-    pub fn generate_share<R: RngCore + CryptoRng>(&mut self, rng: &mut R) -> Result<(), String> {
-
-        let shares = self
-            .participants
-            .iter()
-            .map(|(id, pubkey)| {
-                let secret_share = self.private_poly.evaluate(Scalar::from_bytes(id).unwrap());
-                let public_share = G2Affine::from(G2Affine::generator() * secret_share);
-                let esh = EncryptedShare::new(rng, id, pubkey, &secret_share);
-                Share {
-                    vk: public_share,
-                    esh,
-                }
-            })
-            .collect::<Vec<Share>>();
-        self.shares.insert(self.address, shares);
-        Ok(())
-    }
-
-    pub fn publish_share(&self) -> Option<Vec<Share>> {
-        self.shares.get(&self.address).cloned()
+impl Node<ShareCollection> {
+    pub fn publish_share(&self) -> Vec<Share> {
+        // NOTE unwrap is fine because at this phase, we have
+        // definitely generated our own share when converting
+        // from Discovery phase
+        self.phase.shares.get(&self.address).cloned().unwrap()
     }
 
     pub fn collect_share(&mut self, address: Address, shares: Vec<Share>) {
-        if self.shares.get(&address).is_none() {
-            self.shares.insert(address, shares);
+        if self.phase.shares.get(&address).is_none() {
+            self.phase.shares.insert(address, shares);
         }
     }
 
-    pub fn verify_shares(&self) -> bool {
-        for shares in self.shares.values() {
-            for (id, share) in self.shares.keys().zip(shares) {
-                if !share.esh.verify(id, &share.vk) {
+    fn verify_shares(&self) -> bool {
+        for shares in self.phase.shares.values() {
+            for (address, share) in self.phase.shares.keys().zip(shares) {
+                if !share.esh.verify(address.as_bytes(), &share.vk) {
                     return false;
                 }
             }
@@ -226,54 +230,19 @@ impl TryFrom<Node<GenerateShare>>
         true
     }
 
-    pub fn recover_keys(&mut self) -> Result<(), String> {
-        if self.shares.len() < self.nodes {
-            return Err("not enough shares collected".to_string());
-        } else if self.shsk.is_some() || self.shvk.is_some() || self.gshvk.is_some() {
-            return Err("share signing key already recovered".to_string());
-        }
+    fn interpolated_shvks(&self, address_scalars: &[Scalar]) -> Result<Vec<G2Projective>, String> {
+        let mut interpolated_shvks =
+            Vec::<G2Projective>::with_capacity(self.phase.participants.len());
 
-        let mut self_index = None;
-        let id_scalars = self
-            .shares
-            .keys()
-            .enumerate()
-            .map(|(i, address)| {
-                if address == &self.address {
-                    self_index = Some(i)
-                }
-                // NOTE unwrap is fine because all stored address
-                // come from Scalars originally
-                Scalar::from_bytes(address).unwrap()
-            })
-            .collect::<Vec<Scalar>>();
-
-        // NOTE unwrap is fine because self_index is already in
-        // the storage at this point
-        let self_index = self_index.unwrap();
-        let decrypted_shsks = self.decrypted_shsks(self_index);
-        let interpolated_shvks = self.interpolated_shvks(&id_scalars)?;
-
-        let shsk_poly =
-            Polynomial::interpolate(&id_scalars, &decrypted_shsks).map_err(|e| e.to_string())?;
-        let gshvk_poly =
-            Polynomial::interpolate(&id_scalars, &interpolated_shvks).map_err(|e| e.to_string())?;
-        self.shsk = Some(shsk_poly.coeffs()[0]);
-        self.shvk = Some(interpolated_shvks[self_index].into());
-        self.gshvk = Some(gshvk_poly.coeffs()[0].into());
-        Ok(())
-    }
-
-    fn interpolated_shvks(&self, id_scalars: &[Scalar]) -> Result<Vec<G2Projective>, String> {
-        let mut interpolated_shvks = Vec::<G2Projective>::with_capacity(self.participants.len());
-
-        for i in 0..self.shares.len() {
+        for i in 0..self.phase.shares.len() {
             let shvks = self
+                .phase
                 .shares
                 .values()
                 .map(|vec| vec[i].vk.into())
                 .collect::<Vec<G2Projective>>();
-            let poly = Polynomial::interpolate(&id_scalars, &shvks).map_err(|e| e.to_string())?;
+            let poly =
+                Polynomial::interpolate(&address_scalars, &shvks).map_err(|e| e.to_string())?;
             interpolated_shvks.push(poly.coeffs()[0]);
         }
 
@@ -281,31 +250,91 @@ impl TryFrom<Node<GenerateShare>>
     }
 
     fn decrypted_shsks(&self, self_index: usize) -> Vec<Scalar> {
-        let mut decrypted_shares_for_self = Vec::<Scalar>::with_capacity(self.participants.len());
-        // NOTE unwrap is fine because...
-        for share_vec in self.shares.values() {
+        let mut decrypted_shares_for_self =
+            Vec::<Scalar>::with_capacity(self.phase.participants.len());
+        for share_vec in self.phase.shares.values() {
             decrypted_shares_for_self.push(
                 share_vec[self_index]
                     .esh
-                    .decrypt(&self.address, &self.private_key),
+                    .decrypt(self.address.as_bytes(), &self.keypair.private),
             );
         }
         decrypted_shares_for_self
     }
 
-    pub fn sign(&self, msg: &[u8]) -> G1Affine {
-        let msg_hash_g1 = crate::hash::hash_to_g1(msg);
-        // TODO unwrap -> fix with phases
-        (msg_hash_g1 * self.shsk.unwrap()).into()
-    }
+    fn recover_keys(self) -> Result<Node<Finalized>, String> {
+        let mut self_index = None;
+        let id_scalars = self
+            .phase
+            .shares
+            .keys()
+            .enumerate()
+            .map(|(i, address)| {
+                if address == &self.address {
+                    self_index = Some(i)
+                }
+                address.to_scalar()
+            })
+            .collect::<Vec<Scalar>>();
 
-    // TODO reshare keys
-    // TODO sign message
+        let self_index = self_index.ok_or("self index not found in storage".to_string())?;
+
+        let decrypted_shsks = self.decrypted_shsks(self_index);
+        let interpolated_shvks = self.interpolated_shvks(&id_scalars)?;
+
+        let shsk_poly =
+            Polynomial::interpolate(&id_scalars, &decrypted_shsks).map_err(|e| e.to_string())?;
+        let gshvk_poly =
+            Polynomial::interpolate(&id_scalars, &interpolated_shvks).map_err(|e| e.to_string())?;
+
+        Ok(Node {
+            parameters: self.parameters,
+            address: self.address,
+            keypair: self.keypair,
+            phase: Finalized {
+                participants: self.phase.participants,
+                poly_secret: self.phase.poly_secret,
+                share_keypair: Keypair::new_checked(
+                    shsk_poly.coeffs()[0],
+                    interpolated_shvks[self_index].into(),
+                )?,
+                global_vk: gshvk_poly.coeffs()[0].into(),
+            },
+        })
+    }
 }
 
-pub fn sig_verify(msg: &[u8], vk: &G2Affine, sig: &G1Affine) -> bool {
+impl TryFrom<Node<ShareCollection>> for Node<Finalized> {
+    type Error = String;
+    fn try_from(node: Node<ShareCollection>) -> Result<Self, Self::Error> {
+        if node.phase.shares.len() < node.parameters.nodes {
+            return Err("not enough shares collected".to_string());
+        } else if !node.verify_shares() {
+            return Err("invalid shares collected".to_string());
+        }
+        node.recover_keys()
+    }
+}
+
+impl Node<Finalized> {
+    pub fn verifying_key(&self) -> G2Affine {
+        self.phase.share_keypair.public
+    }
+
+    pub fn partial_sign(&self, msg: &[u8]) -> G1Affine {
+        self.phase.share_keypair.sign(msg)
+    }
+}
+
+// TODO reshare keys
+
+pub fn partial_sig_verify(msg: &[u8], vk: &G2Affine, sig: &G1Affine) -> bool {
     let msg_hash_g1 = crate::hash::hash_to_g1(msg);
     bls::pairing(&msg_hash_g1, vk) == bls::pairing(sig, &G2Affine::generator())
+}
+
+pub fn global_sig_verify() -> bool {
+    todo!();
 }
 
 #[cfg(test)]
@@ -315,59 +344,62 @@ mod test {
     #[test]
     fn dkg_23() {
         let mut rng = rand_core::OsRng;
-        let nodes = 3_usize;
-        let threshold = 2_usize;
+        let parameters = Parameters::new(3, 2);
         // spin up nodes
-        let mut node_0 = Node::new(rng, nodes, threshold);
-        let mut node_1 = Node::new(rng, nodes, threshold);
-        let mut node_2 = Node::new(rng, nodes, threshold);
+        let mut node_0 = Node::<Discovery>::new(parameters, Keypair::random(&mut rng));
+        let mut node_1 = Node::<Discovery>::new(parameters, Keypair::random(&mut rng));
+        let mut node_2 = Node::<Discovery>::new(parameters, Keypair::random(&mut rng));
         // collect participants
-        node_0.collect_participant(node_1.address, node_1.public_key);
-        node_0.collect_participant(node_2.address, node_2.public_key);
-        node_1.collect_participant(node_0.address, node_0.public_key);
-        node_1.collect_participant(node_2.address, node_2.public_key);
-        node_2.collect_participant(node_0.address, node_0.public_key);
-        node_2.collect_participant(node_1.address, node_1.public_key);
+        node_0.collect_participant(node_1.pubkey());
+        node_0.collect_participant(node_2.pubkey());
+        node_1.collect_participant(node_0.pubkey());
+        node_1.collect_participant(node_2.pubkey());
+        node_2.collect_participant(node_0.pubkey());
+        node_2.collect_participant(node_1.pubkey());
         // generate partial shares
-        node_0.generate_share(&mut rng).unwrap();
-        node_1.generate_share(&mut rng).unwrap();
-        node_2.generate_share(&mut rng).unwrap();
+        let mut node_0 = Node::<ShareCollection>::try_from(node_0).unwrap();
+        let mut node_1 = Node::<ShareCollection>::try_from(node_1).unwrap();
+        let mut node_2 = Node::<ShareCollection>::try_from(node_2).unwrap();
         // publish and collect shares
-        node_0.collect_share(node_1.address, node_1.publish_share().unwrap());
-        node_0.collect_share(node_2.address, node_2.publish_share().unwrap());
-        node_1.collect_share(node_0.address, node_0.publish_share().unwrap());
-        node_1.collect_share(node_2.address, node_2.publish_share().unwrap());
-        node_2.collect_share(node_0.address, node_0.publish_share().unwrap());
-        node_2.collect_share(node_1.address, node_1.publish_share().unwrap());
-        assert_eq!(node_0.participants.len(), nodes);
-        assert_eq!(node_1.participants.len(), nodes);
-        assert_eq!(node_2.participants.len(), nodes);
-        assert_eq!(node_0.shares.len(), nodes);
-        assert_eq!(node_1.shares.len(), nodes);
-        assert_eq!(node_2.shares.len(), nodes);
+        node_0.collect_share(node_1.address(), node_1.publish_share());
+        node_0.collect_share(node_2.address(), node_2.publish_share());
+        node_1.collect_share(node_0.address(), node_0.publish_share());
+        node_1.collect_share(node_2.address(), node_2.publish_share());
+        node_2.collect_share(node_0.address(), node_0.publish_share());
+        node_2.collect_share(node_1.address(), node_1.publish_share());
+        assert_eq!(node_0.phase.participants.len(), parameters.nodes);
+        assert_eq!(node_1.phase.participants.len(), parameters.nodes);
+        assert_eq!(node_2.phase.participants.len(), parameters.nodes);
+        assert_eq!(node_0.phase.shares.len(), parameters.nodes);
+        assert_eq!(node_1.phase.shares.len(), parameters.nodes);
+        assert_eq!(node_2.phase.shares.len(), parameters.nodes);
         // verify collected shares
-        assert!(node_0.verify_shares());
-        assert!(node_1.verify_shares());
-        assert!(node_2.verify_shares());
-        // recover signing and verification keys
-        node_0.recover_keys().unwrap();
-        node_1.recover_keys().unwrap();
-        node_2.recover_keys().unwrap();
-        assert_eq!(node_0.gshvk, node_1.gshvk);
-        assert_eq!(node_1.gshvk, node_2.gshvk);
-        // assert that keypairs belong together
-        assert_eq!(
-            node_0.shvk.unwrap(),
-            G2Affine::from(G2Affine::generator() * node_0.shsk.unwrap())
-        );
-        assert_eq!(
-            node_1.shvk.unwrap(),
-            G2Affine::from(G2Affine::generator() * node_1.shsk.unwrap())
-        );
-        assert_eq!(
-            node_2.shvk.unwrap(),
-            G2Affine::from(G2Affine::generator() * node_2.shsk.unwrap())
-        );
+        let node_0 = Node::<Finalized>::try_from(node_0).unwrap();
+        let node_1 = Node::<Finalized>::try_from(node_1).unwrap();
+        let node_2 = Node::<Finalized>::try_from(node_2).unwrap();
+        assert_eq!(node_0.phase.global_vk, node_1.phase.global_vk);
+        assert_eq!(node_1.phase.global_vk, node_2.phase.global_vk);
+        // sign message
+        let msg = b"hello world";
+        let signatures = vec![
+            node_0.partial_sign(msg),
+            node_1.partial_sign(msg),
+            node_2.partial_sign(msg),
+        ];
+        assert!(partial_sig_verify(
+            msg,
+            &node_0.verifying_key(),
+            &signatures[0]
+        ));
+        assert!(partial_sig_verify(
+            msg,
+            &node_1.verifying_key(),
+            &signatures[1]
+        ));
+        assert!(partial_sig_verify(
+            msg,
+            &node_2.verifying_key(),
+            &signatures[2]
+        ));
     }
 }
-*/
